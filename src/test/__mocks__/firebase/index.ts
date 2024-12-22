2d33a76396bec8aec2e6da166310b9ce6c818617
tveigel <timothyvau@gmail.com>
2024-12-21 22:54:16 -0600

    restructured test env and deleted all previous tests

--- src/test/__mocks__/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-// src/test/__mocks__/firebase/index.ts
-export { mockAuth } from './auth';
-export { mockFirestore, mockTimestamp } from './firestore';
-
-// Mock the actual Firebase config
-jest.mock('@/lib/firebase/firebaseConfig', () => ({
-  auth: mockAuth,
-  db: mockFirestore,
-}));
